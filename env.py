463b0c15e55594e127f84e9e3053040b4a758221
mochfatchur <13519009@std.stei.itb.ac.id>
2024-11-10 17:46:16 +0700

    update: generate dinamic associated data (AD)

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ wifi_ssid = ""
 wifi_password = ""
 mqtt_broker_ip = ""
 topic = b""
+delay_send = 1
+
--- publisher.py
+++ publisher.py
@@ -1,4 +1,4 @@
-from env import mqtt_broker_ip, topic
+from env import mqtt_broker_ip, topic, delay_send
 import time
 import ubinascii
 import machine
@@ -11,8 +11,7 @@ import json
 # crypt lib
 import ascon
 
-
-associateddata = b""
+# crypt cred
 variant = "Ascon-128"
 
 # Default MQTT server to connect to
@@ -38,30 +37,46 @@ def main():
     print("Connected to MQTT  Broker :: ", SERVER)
 
     while True:
+        # read data sensor
         random_temp = random.randint(20, 50)
+
+        # generate key and nonce/IV
         nonce = os.urandom(16)
         key = os.urandom(16)
+
         # Get plaintext
         plaintext = str(random_temp).encode('utf-8')
+
+        # Generate associated data
+        device_id = CLIENT_ID.decode('utf-8')  # Device ID based on machine unique ID
+        timestamp = str(int(time.time()))  # Current timestamp as a string
+        sensor_type = "ecg"  # Data type description
+
+        # Format associated data string
+        associateddata = (device_id + "|" + timestamp + "|" + sensor_type).encode('utf-8')
+
         # Encrypt
         ciphertext = ascon.ascon_encrypt(key, nonce, associateddata, plaintext, variant)
+
         # Mengonversi ciphertext, nonce, dan public key ke Base64
         ciphertext_base64 = base64_encode(ciphertext)
         nonce_base64 = base64_encode(nonce)
         public_key_base64 = base64_encode(key)
+
         # format payload
         payload = {
             "data": ciphertext_base64,
             "nonce": nonce_base64,
-            "public_key": public_key_base64
+            "public_key": public_key_base64,
+            'associated_data': associateddata
         }
-        print("test payload: ", payload)
+
         # Publishing data
-        print("Publishing temperature :: ", random_temp)
-        print("Publishing temperature :: ", ciphertext_base64)
+        print("Publishing ECG :: ", random_temp)
         mqttClient.publish(TOPIC, json.dumps(payload))
-        # delay??
-        time.sleep(3)
+
+        # delay
+        time.sleep(delay_send)
     mqttClient.disconnect()
 
 
